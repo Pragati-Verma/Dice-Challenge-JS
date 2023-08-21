f359712d7025e7ea6889ee1c7c06ef4bc3573475
Pragati-Verma <30720974+Pragati-Verma@users.noreply.github.com>
2023-08-21 15:52:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomImage1 = "dice" + randomNumber1 + ".png";
+var randomImageSource1 = "./images/" + randomImage1;
+var elmt1 = document.querySelectorAll("img")[0];
+elmt1.setAttribute("src",randomImageSource1);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomImage2 = "dice" + randomNumber2 + ".png";
+var randomImageSource2 = "./images/" + randomImage2;
+var elmt2 = document.querySelectorAll("img")[1];
+elmt2.setAttribute("src",randomImageSource2);
+
+if(randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 Wins";
+}
+
+else if(randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins";
+}
+
+else { 
+    document.querySelector("h1").innerHTML = "Draw!!!!";
+}
